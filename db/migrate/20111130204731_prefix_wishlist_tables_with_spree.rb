95b3ade9e07fc7e5944bc7c0fb08ba4b665a5100
arvind0143 <arvind.jangir@bluebash.co>
2023-09-21 18:00:55 +0530

    made spree wishlist gem compatible with spree 4.6.

--- db/migrate/20111130204731_prefix_wishlist_tables_with_spree.rb
+++ db/migrate/20111130204731_prefix_wishlist_tables_with_spree.rb
@@ -1,6 +1,6 @@
 class PrefixWishlistTablesWithSpree < SpreeExtension::Migration[4.2]
   def change
-    rename_table :wishlists, :spree_wishlists
+    # rename_table :wishlists, :spree_wishlists
     rename_table :wished_products, :spree_wished_products
   end
 end

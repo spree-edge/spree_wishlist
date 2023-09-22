d330442a11874790238dfd5b978a80a1ba1f52dc
arvind0143 <arvind.jangir@bluebash.co>
2023-09-22 10:17:31 +0530

    minor fix.

--- db/migrate/20210729053613_change_user_id_type_for_spree_wishlists.rb
+++
@@ -1,7 +0,0 @@
-class ChangeUserIdTypeForSpreeWishlists < ActiveRecord::Migration[4.2]
-  def change
-    change_table(:spree_wishlists) do |t|
-      t.change :user_id, :bigint
-    end
-  end
-end

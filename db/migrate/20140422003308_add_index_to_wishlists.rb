8c77a93b5e0585e67431ec020fc17e8f3c3d93ff
arvind0143 <arvind.jangir@bluebash.co>
2023-09-22 10:11:02 +0530

    removed unwanted migrations.

--- db/migrate/20140422003308_add_index_to_wishlists.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToWishlists < SpreeExtension::Migration[4.2]
-  def change
-    add_index :spree_wishlists, [:user_id]
-    add_index :spree_wishlists, [:user_id, :is_default]
-  end
-end

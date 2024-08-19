81786e6309dcc261e1de8ce814a4aed4b06d5767
Shrey <shrey.gupta@bluebash.co>
2024-08-19 15:14:45 +0530

    Removed deface dependency

--- app/overrides/add_wish_to_cart_form.rb
+++
@@ -1,8 +0,0 @@
-ActiveSupport::Deprecation.warn('"Add to wishlist" button is deprecated and and is not working properly.')
-
-Deface::Override.new(
-  virtual_path: 'spree/products/show',
-  name: 'add_wish_to_cart_form',
-  insert_bottom: "[data-hook='cart_form']",
-  partial: 'spree/products/wishlist_form'
-)
